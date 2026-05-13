40fb36f219a41c576d421cf5c0bea20ce3c5c75b
Jovansa Putra Laksana <jovansaputralaksana@gmail.com>
2026-05-13 16:33:47 +0700

    context with deadline

--- solusi_goroutineLeak_test.go
+++ solusi_goroutineLeak_test.go
@@ -60,3 +60,16 @@ func TestContextWithTimeout(t *testing.T) {
 
 	fmt.Println("Total Goroutine", runtime.NumGoroutine())
 }
+
+func TestContextWithDeadline(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(5*time.Second))
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
